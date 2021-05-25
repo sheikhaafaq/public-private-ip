7c439acdbbcdfec2fc7fc3e0b4a95fd7bedb7b27
sheikh aafaq <sheikhaafaq18@gmail.com>
2021-05-25 10:59:07 +0530

    latest

--- private_ip.py
+++ private_ip.py
@@ -4,3 +4,5 @@ def privateIP():
   hostname = socket.gethostname()
   local_ip = socket.gethostbyname(hostname)
   return local_ip
+
+print(privateIP())
--- public_ip.py
+++ public_ip.py
@@ -3,3 +3,5 @@ from requests import get
 def publicIP():
   ip = get('https://api.ipify.org').text
   return ip
+
+print(publicIP())
